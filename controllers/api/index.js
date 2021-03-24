46b2c23765ad85b4c3f67b6ef0189ebb7f607078
jarred <jarred.caris@gmail.com>
2021-03-24 19:27:17 +1030

    changes to get files to connect

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes')
+const userRoutes = require('./userRoutes')
+const postRoutes = require('./postRoutes')
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes)
+router.use('/user', userRoutes)
+router.use('/post', postRoutes)
 
 module.exports = router;
