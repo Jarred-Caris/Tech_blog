b6686c656e2a5eb247be6ece9735d3ace976743d
jarred <jarred.caris@gmail.com>
2021-04-05 11:45:53 +0930

    added comment routes from prev activity and modified. added put option in existing code

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,37 +1,62 @@
-const router = require('express').Router();
-const { Post } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
-      const newpost = await Post.create({
-          ...req.body,
-          user_id: req.session.user_id,
-      });
+    const newpost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
-      res.status(200).json(newPost);
+    res.status(200).json(newPost);
   } catch (err) {
-      res.status(400).json(err);
+    res.status(400).json(err);
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete("/:id", withAuth, async (req, res) => {
   try {
-      const postData = await Post.destroy({
-          where: {
-              id: req.params.id,
-              user_id: req.session.user_id,
-          },
-      });
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-      if (!postData) {
-          res.status(404).json({ message: 'Post not found' });
-          return;
-      }
+    if (!postData) {
+      res.status(404).json({ message: "Post not found" });
+      return;
+    }
 
-      res.status(200).json(postData);
+    res.status(200).json(postData);
   } catch (err) {
-      res.status(500).json(err);
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    if (!req.session.loggedIn) {
+      res.redirect("/login");
+      return;
+    }
+    const updatePost = await Post.update(
+      {
+        where: {
+          id: req.params.id,
+        },
+      },
+      {
+        title: req.body.title,
+        post: req.body.post,
+      }
+    );
+
+    res.status(200).json(updatePost);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json(error);
   }
 });
 
