9888c4f6e2921fe539b1319ab90bb1f1523713c0
jarred <jarred.caris@gmail.com>
2021-04-05 13:45:50 +0930

    profile js file created

---
+++ public/JS/profile.js
@@ -0,0 +1,30 @@
+const newPostHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#post-name').value.trim();
+  
+  const description = document.querySelector('#post-desc').value.trim();
+
+  if (name && description) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ name, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create project');
+    }
+  }
+};
+
+
+document
+  .querySelector('.new-project-form')
+  .addEventListener('submit', newFormHandler);
+
+
